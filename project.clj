35f3a35b252fdbf4b6dfc7f2a1567b229e53e2da
James Petry <gavilancomun@gmail.com>
2015-10-07 10:24:29 +0100

    antlr update

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject clj-antlr "0.2.2"
+(defproject clj-antlr "0.2.3"
   :description "Clojure bindings for the ANTLR 4 parser library."
-  :url "http://github.com/aphyr/clj-antlr"
+  :url "http://github.com/gavilancomun/clj-antlr"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.antlr/antlr4-runtime "4.2.2"]
-                 [org.antlr/antlr4 "4.2.2"]]
+                 [org.antlr/antlr4-runtime "4.5.1-1"]
+                 [org.antlr/antlr4 "4.5.1-1"]]
   :profiles {:dev {:dependencies
                    [[criterium "0.4.0"]
                     [cheshire "5.3.1"]
